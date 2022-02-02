76db74133958ce0b343e2eef322b61e9f016389b
takizawazaku <96064983+takizawazaku@users.noreply.github.com>
2022-02-02 23:49:31 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -191,3 +191,20 @@
 //   }
 // });
 // console.log(newNameArr); // ["田中さん", "山田さん", "じゃけぇ"]
+
+/**
+ * 三項演算子 ※ES2015からではないがReactでよく使うので解説
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = 1300;
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です";
+// };
+// console.log(checkSum(10, 90));
